302f92760d6d2340b967ca0c3c8d0e5f4f928fcc
Vincent Post <v.post@condifood.com>
2020-02-15 21:46:37 +0100

    ADD: level object to load in level and interpret characters as game objects

---
+++ src/level.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "dot.h"
+
+#include "SDL.h"
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+
+class Level {
+public:
+  Level(std::size_t const screen_width, std::size_t const screen_height, std::size_t const grid_width, std::size_t const grid_height);
+  std::vector<Dot> dots_;
+  std::vector<SDL_Rect> walls_;
+  SDL_Point player_ = {-1, -1};
+
+  void convertCharacterToGameObject(char const character, unsigned int x, unsigned int y);
+  void load(std::string directory = "levels", std::string filename = "default");
+
+private:
+  std::size_t const screen_width_;
+  std::size_t const screen_height_;
+  std::size_t const grid_width_;
+  std::size_t const grid_height_;
+
+
+};
