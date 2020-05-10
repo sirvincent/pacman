3d57365f137f2dd12a7fcf38b842450e8b22823a
Vincent Post <v.post@condifood.com>
2020-05-10 16:47:37 +0200

    ADD: obtain the executable path via argv
    Not optimal since it is not robust, see comments in code

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 
 #include <cassert>
 #include <iostream>
+#include <cstring>
+#include <filesystem>
+
+#define ASSERT_MESSAGE(exp, msg) assert(((void)msg, exp))
 
 
-int main() {
+int main(int argc, char **argv) {
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kGridWidth{32};
@@ -14,6 +18,18 @@ int main() {
   // The reason for - kGridWidth is since we want to have a middle in the level
   constexpr std::size_t kScreenWidth{640 - kGridWidth};
   constexpr std::size_t kScreenHeight{640 + kGridHeight};
+
+  // TODO: We should robustly obtain the executable path, argv[0] might fail.
+  //       How do we do this? use Poco? Boost? whereami (https://github.com/gpakosz/whereami)?
+  //       Preferably something OS independend
+  // following the C++ ISO standard if argv[0]:
+  // argv[0] shall be the pointer to the initial character of a null-terminated multibyte string
+  // that represents the name used to invoke the program or ""
+  // if argc is nonzero
+  ASSERT_MESSAGE(argc != 0, "argc == 0 hence extracting the path name of executable will not work");
+  ASSERT_MESSAGE(std::strcmp(argv[0], ""), "argv is empty hence executable path can not be found");
+
+  std::filesystem::path executable_path = std::filesystem::path(argv[0]);
 
   assert(kScreenWidth % kGridWidth == 0);
   assert(kScreenHeight % kGridHeight == 0);
